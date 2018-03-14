3eef7a672c2e8632061dedecb5597b5d1845445c
aliona <alegroma@gmail.com>
2018-03-14 18:29:29 +0100

    Redone the graph, looks palatable

--- js/dijkstra.js
+++ js/dijkstra.js
@@ -1,87 +1,146 @@
-init();
+forceLayout();
 
-function init() {
-    var vis = d3.select("#graph")
-        .append("svg");
+function forceLayout() {
+
+    // http://bl.ocks.org/sathomas/11550728
+    // https://stackoverflow.com/questions/28847443/adding-text-labels-to-force-directed-graph-links-in-d3-js
+    // https://stackoverflow.com/questions/28050434/introducing-arrowdirected-in-force-directed-graph-d3
+    // https://hackernoon.com/how-to-implement-dijkstras-algorithm-in-javascript-abdfd1702d04
+    // https://stackoverflow.com/questions/37640027/add-text-label-to-d3-node-in-force-layout
 
     var graph = $("#graph");
-    var w = graph.width(),
-        h = graph.height();
-    vis.attr("width", w)
-        .attr("height", h);
+    var width = graph.width(),
+        height = graph.height();
 
-    vis.text("The Graph")
-        .select("#graph");
+    var svg = d3.select('#graph').append('svg')
+        .attr('width', width)
+        .attr('height', height);
 
-    var nodesX = w - 20;
-    var nodesY = h - 20;
+    var nodesX = width - 20;
+    var nodesY = height - 20;
     var nodes = [
-        {x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
-        {x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
-        {x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
-        {x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
-        {x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
-        {x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)}
-        ];
-
-    vis.selectAll("circle .nodes")
-        .data(nodes)
-        .enter()
-        .append("svg:circle")
-        .attr("class", "nodes")
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-
-    vis.selectAll("circle .nodes")
-        .data(nodes)
-        .enter()
-        .append("svg:circle")
-        .attr("class", "nodes")
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .attr("r", "10px")
-        .attr("fill", "black");
-
-    var links = [
-        {source: nodes[0], target: nodes[1], weight: Math.floor(Math.random() * 10)},
-        {source: nodes[2], target: nodes[1], weight: Math.floor(Math.random() * 10)},
-        {source: nodes[3], target: nodes[0], weight: Math.floor(Math.random() * 10)},
-        {source: nodes[4], target: nodes[2], weight: Math.floor(Math.random() * 10)},
-        {source: nodes[1], target: nodes[4], weight: Math.floor(Math.random() * 10)},
-        {source: nodes[5], target: nodes[4], weight: Math.floor(Math.random() * 10)},
-        {source: nodes[5], target: nodes[2], weight: Math.floor(Math.random() * 10)}
+        {name: "A", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "B", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "C", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "D", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "E", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "F", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "G", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "H", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "I", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "J", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)},
+        {name: "K", x: (Math.floor(Math.random() * nodesX) + 10), y: (Math.floor(Math.random() * nodesY) + 10)}
     ];
 
-    var link = vis.selectAll(".link")
+    // var links = [
+    //     {source: nodes[0], target: nodes[1], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[2], target: nodes[1], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[3], target: nodes[0], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[4], target: nodes[2], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[1], target: nodes[4], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[5], target: nodes[4], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[5], target: nodes[2], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[5], target: nodes[0], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[1], target: nodes[0], weight: Math.floor(Math.random() * 10)},
+    //     {source: nodes[2], target: nodes[3], weight: Math.floor(Math.random() * 10)}
+    // ];
+
+    var links = [
+        {source: nodes[0], target: nodes[1], weight: 85},
+        {source: nodes[0], target: nodes[2], weight: 217},
+        {source: nodes[0], target: nodes[4], weight: 173},
+        {source: nodes[2], target: nodes[6], weight: 186},
+        {source: nodes[2], target: nodes[7], weight: 103},
+        {source: nodes[3], target: nodes[7], weight: 183},
+        {source: nodes[5], target: nodes[8], weight: 250},
+        {source: nodes[8], target: nodes[9], weight: 85},
+        {source: nodes[7], target: nodes[9], weight: 167},
+        {source: nodes[4], target: nodes[9], weight: 502},
+        {source: nodes[9], target: nodes[10], weight: 40},
+        {source: nodes[1], target: nodes[10], weight: 600}
+    ];
+
+    var force = d3.layout.force()
+        .size([width, height])
+        .nodes(nodes)
+        .links(links);
+
+    force.linkDistance(function(d) {
+        return Math.floor(Math.random() * (width/1.5));
+    });
+
+    svg.append("svg:defs").selectAll("marker")
+        .data(["end"])      // Different link/path types can be defined here
+        .enter()
+        .append("svg:marker")    // This section adds in the arrows
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -1.5)
+        .attr("markerWidth", 5)
+        .attr("markerHeight", 5)
+        .attr("orient", "auto")
+        .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+    var link = svg.selectAll('.link')
         .data(links)
         .enter()
         .append("g")
-        .attr("class", "link");
+        .attr("class", "link-group")
+        .append('line')
+        .attr('class', 'link')
+        .attr("marker-end", "url(#end)");
 
-    link.append("line")
-        .attr("fill", "none")
-        .attr("stroke", "#ff8888")
-            .attr("x1", function(d) { return d.source.x })
-            .attr("y1", function(d) { return d.source.y })
-            .attr("x2", function(d) { return d.target.x })
-            .attr("y2", function(d) { return d.target.y })
-        .attr("stroke-width", "1.5px");
+    var linkText = svg.selectAll(".link-group")
+        .append("text")
+        .data(force.links())
+        .text(function(d) { return d.weight; })
+        .attr("x", function(d) { return (d.source.x + (d.target.x - d.source.x) * 0.5); })
+        .attr("y", function(d) { return (d.source.y + (d.target.y - d.source.y) * 0.5); })
+        .attr("dy", ".25em")
+        .attr("text-anchor", "middle");
 
-    link.append("text")
-        .attr("font-family", "Arial, Helvetica, sans-serif")
-        .attr("fill", "Black")
-        .style("font", "normal 12px Arial")
-        .attr("transform", function(d) {
-            return "translate(" +
-                ((d.source.x + d.target.x)/2) + "," +
-                ((d.source.y + d.target.y)/2) + ")";
-        })
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .text(function(d) {
-            console.log("success");
-            return d.weight;
-        });
+    var node = svg.selectAll('.node')
+        .data(nodes)
+        .enter().append('circle')
+        .attr('class', 'node');
+
+    var label = svg.selectAll(".label")
+        .data(nodes)
+        .enter()
+        .append("text")
+        .text(function (d) { return d.name; })
+        .attr('class', 'label')
+        .style("text-anchor", "middle")
+        .style("fill", "#555")
+        .style("font-family", "Arial")
+        .style("font-size", 12);
+
+    force.on('tick', function() {
+        node.attr('r', width/100)
+            .attr('cx', function(d) { return d.x; })
+            .attr('cy', function(d) { return d.y; });
+
+        link.attr('x1', function(d) { return d.source.x; })
+            .attr('y1', function(d) { return d.source.y; })
+            .attr('x2', function(d) { return d.target.x; })
+            .attr('y2', function(d) { return d.target.y; });
+
+        linkText
+            .attr("x", function(d) { return (d.source.x + (d.target.x - d.source.x) * 0.5); })
+            .attr("y", function(d) { return (d.source.y + (d.target.y - d.source.y) * 0.5); });
+
+        label.attr("x", function(d){ return d.x; })
+            .attr("y", function (d) {return d.y - 10; });
+
+    });
+
+    force.start();
+}
+
+function dijkstra() {
+    //TODO: doobiedoo
 }
 
 
