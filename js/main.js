a6398eb4693601619f753a333b231d42c6efae07
Hedenius <winqvist.martin@gmail.com>
2018-03-16 11:15:26 +0100

    side menu collapses on small and medium screen sizes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-// Add js that all pages use
+$(document).ready(function () {
+    if (matchMedia) {
+        var mq = window.matchMedia("(min-width: 1024px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+    }
+
+    function WidthChange(mq) {
+        if (mq.matches) {
+            $("#algorithms").addClass("show");
+            $("#datastructures").addClass("show");
+        }
+        else {
+            $("#algorithms").removeClass("show");
+            $("#datastructures").removeClass("show");
+        }
+    }
+});
